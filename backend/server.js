121efa2cc4e0fcfe73d2c723891aa13704ab8095
Shubham yadav <140435929+code0era@users.noreply.github.com>
2025-11-28 16:41:36 +0530

    Payment and Analytics

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,11 @@ import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js";
 import productRoutes from "./routes/product.route.js";
 import cartRoutes from "./routes/cart.route.js";
+import couponRoutes from "./routes/coupon.route.js";
+import paymentRoutes from "./routes/payment.route.js";
+import analyticsRoutes from "./routes/analytics.route.js";
+
+
 import { connectDB } from "./lib/db.js";
 
 dotenv.config();
@@ -14,10 +19,11 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json()); // parsing
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
-app.use("/api/products", productRoutes);
+app.use("/api/products", productRoutes); 
 app.use("/api/cart", cartRoutes);
 app.use("/api/coupons", couponRoutes);
-app.use("/api/payments", paymentsRoutes);
+app.use("/api/payments", paymentRoutes);
+app.use("/api/analytics", analyticsRoutes);
 
 app.listen(PORT, () => {
   console.log("Server is running on port ", PORT);
